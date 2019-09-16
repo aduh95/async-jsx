49681b1d1fa917af636fe2152a4bcb0091137f45
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-09-16 19:43:10 -0400

    renderAsync: simplify error handling with no placeholder

--- src/renderAsync/renderAsync.ts
+++ src/renderAsync/renderAsync.ts
@@ -2,10 +2,17 @@ import AsyncElement, { ASYNC_WRAPPER } from "./AsyncElement";
 
 import { AsyncAppendable, SynchroneAppendable } from "./StateCollection";
 
+type FallbackCallback = (
+  e: Error
+) => AsyncAppendable | SynchroneAppendable | void;
+
+/**
+ * renderAsync(asyncElement[, placeholder][, onError])
+ */
 export default function renderAsync(
   asyncElement: AsyncAppendable,
-  placeholder?: ChildNode | Promise<ChildNode>,
-  fallback?: (e: Error) => AsyncAppendable | SynchroneAppendable | void
+  placeholder?: ChildNode | Promise<ChildNode> | FallbackCallback,
+  onError?: FallbackCallback
 ) {
   const wrapper = document.createElement(ASYNC_WRAPPER) as AsyncElement;
   const { prototype } = AsyncElement;
@@ -18,13 +25,15 @@ export default function renderAsync(
       .catch(console.warn);
   } else if ((placeholder as ChildNode).replaceWith) {
     wrapper.placeholder(placeholder as ChildNode);
+  } else if ("function" === typeof placeholder) {
+    onError = placeholder as FallbackCallback;
   } else {
     throw new TypeError("Unsuitable placeholder provided");
   }
 
   asyncElement
     .then(prototype.commit.bind(wrapper))
-    .catch(fallback)
+    .catch(onError)
     .then(prototype.commit.bind(wrapper))
     .catch(() => {
       wrapper.remove();
