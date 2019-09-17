0d0fb4b2ffd0ed04ee937287ffdc0e4c0bf8e145
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-09-16 22:17:05 -0400

    createSVGElement: fix bugs with SVG

--- src/react/createSVGElement.ts
+++ src/react/createSVGElement.ts
@@ -16,6 +16,20 @@ export default async function h(
       refMap.set(props.ref, element);
       props.ref = undefined;
     }
+    if ("object" === typeof props.style) {
+      Object.entries(props.style as ({
+        [propertyName: string]: string;
+      })).forEach(([propertyName, value]) =>
+        domElement.style.setProperty(propertyName, value)
+      );
+      props.style = undefined;
+    }
+    if (props.className) {
+      // className is read-only on SVGElement
+      // @see https://svgwg.org/svg2-draft/types.html#InterfaceSVGElement
+      domElement.setAttribute("class", props.className);
+      props.className = undefined;
+    }
     Object.entries(props)
       .filter(([name, value]) => value !== undefined)
       .forEach(([name, value]) => domElement.setAttribute(name, value));
@@ -26,5 +40,5 @@ export default async function h(
     domElement.append(...subElements.filter(Boolean));
   }
 
-  return element;
+  return domElement;
 }
