3e45e50fd36b965e473cc26b521f5f22d642e09f
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-09-16 21:23:48 -0400

    createSVGElement: Add SVG factory

---
+++ src/react/createSVGElement.ts
@@ -0,0 +1,30 @@
+import { refMap } from "./createRef";
+import { Props } from "./Component";
+import { ComponentChildren } from "../renderAsync/StateCollection";
+
+const SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+
+export default async function h(
+  element: string,
+  props: Props = null,
+  ...children: (ComponentChildren | ComponentChildren[])[]
+) {
+  const domElement = document.createElementNS(SVG_NAMESPACE, element);
+
+  if (props) {
+    if (props.ref) {
+      refMap.set(props.ref, element);
+      props.ref = undefined;
+    }
+    Object.entries(props)
+      .filter(([name, value]) => value !== undefined)
+      .forEach(([name, value]) => domElement.setAttribute(name, value));
+  }
+
+  if (children.length) {
+    const subElements: Node[] = await Promise.all(children.flat(Infinity));
+    domElement.append(...subElements.filter(Boolean));
+  }
+
+  return element;
+}
